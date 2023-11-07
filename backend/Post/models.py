13eb6088b2d975d3b8bea710899e43f5e28c779d
braguru <bra_guru@protonmail.com>
2023-11-07 11:28:15 +0000

    Add post models and serializers

--- backend/Post/models.py
+++ backend/Post/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    content = models.CharField(max_length=4000)
+    tag = models.CharField(max_length=50, db_index=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
+    is_solved = models.BooleanField(default=False)
+    post_image = models.CharField(max_length=255, blank=True)
+    post_date = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
