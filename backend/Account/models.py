5dd5310447b151f57a4a0f7a5d68fee258697305
JerryAgbesi <jerryeagbesi@gmail.com>
2023-11-07 10:34:37 +0000

    Add user models

--- backend/Account/models.py
+++ backend/Account/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("Users must have a valid student email address")
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self.db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self) -> str:
+        return self.email
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    programme = models.CharField(max_length=255)
+    current_level = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+    profile_picture = models.CharField(max_length=255)
+    bio = models.CharField(max_length=255)
